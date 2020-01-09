827381cc386d321ced34e30e028beb64da63f9a5
Ruolei Xia <xiaruolei@gmail.com>
2020-01-08 22:19:21 -0500

    metaclass

--- chapter04/abc_test.py
+++ chapter04/abc_test.py
@@ -1,4 +1,4 @@
-# 检查某个类是否有某种方法
+# ----1. 检查某个类是否有某种方法----
 class Company(object):
     def __init__(self, employee_list):
         self.employee = employee_list
@@ -13,15 +13,54 @@ print(len(company))
 # method 1
 print(hasattr(company, "__len__"))
 
-# method 2 (判定某个对象的类型)
+# method 2 (判定某个对象的类型): isinstance
 from collections.abc import Sized
 print(isinstance(company, Sized))
 
 
-# 强制某个子类必须实现某些方法
-class CacheBase(object):
+class A:
+    pass
+
+
+class B(A):
+    pass
+
+
+b = B()
+print(isinstance(b, A))
+
+
+# ----2. 强制某个子类必须实现某些方法----
+import abc
+
+
+class CacheBase(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def get(self, key):
+        pass
+
+    @abc.abstractmethod
+    def set(self, key, value):
+        pass
+
+
+# use metaclass, so error will be raised when we create an object rather than call object's function
+# class CacheBase:
+#     def get(self, key):
+#         raise NotImplementedError
+#
+#     def set(self, key, value):
+#         raise NotImplementedError
+
+
+class RedisCache(CacheBase):
     def get(self, key):
         pass
 
     def set(self, key, value):
         pass
+
+
+redis_cache = RedisCache()
+# redis_cache.set("key", "value")
